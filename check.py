f9af09f42ee5734cb4a61e1f68346eb35a402f2b
yjchen0228 <yjchen0228@gmail.com>
2023-12-19 00:26:18 +0800

    add

--- check.py
+++ check.py
@@ -2,6 +2,6 @@ print("hello world")
 
 print("this is in test2 folder")
 print ("that's make some noise and Test2")
-print("this is in Test1 folder")
+print("this is i folder")
 
 print("how to lose")
