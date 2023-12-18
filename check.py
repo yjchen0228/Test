1b7ba53f7e92008cd7622381386bc81f96d5d371
yjchen0228 <yjchen0228@gmail.com>
2023-12-19 00:24:13 +0800

    heyhye

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 print("hello world")
 
 print("this is in test2 folder")
-print ("that's make some noise and conflict")
+print ("that's make some noise and Test2")
 print("this is in Test1 folder")
 
 print("how to lose")
